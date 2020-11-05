cc18f81d017453be674939869a8fe456fcf01e09
dencoSeca <leon.brown71@gmail.com>
2020-11-05 12:43:11 +0000

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="container">
+          <div className="logo">
+            <a href="/">BRAGAZZI'S</a>
+          </div>
+          <div className="menu-button">
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
