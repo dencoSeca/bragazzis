023f62ba6c6f3120554079ce4d3f37dc995a71f9
dencoSeca <leon.brown71@gmail.com>
2020-11-15 12:05:10 +0000

    add axios request for opening hours on cafe page

--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 // Components
 import Page from '../components/Page'
 
 function Cafe() {
+  const [placeData, setPlaceData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyBMnHkqSbbWDI8oipDnotNtzf7AltbSquM&place_id=ChIJXQTkrGGCeUgRSaL1gEOk_MY&fields=opening_hours/weekday_text'
+      )
+      .then(response => {
+        setPlaceData(response.data.result.opening_hours.weekday_text)
+      })
+  }, [])
+
   return (
     <>
-      <Page title="Cafe"></Page>
+      <Page title="Cafe">
+        <div className="cafe">
+          {placeData.map((data, index) => (
+            <p key={index}>{data}</p>
+          ))}
+        </div>
+      </Page>
     </>
   )
 }
