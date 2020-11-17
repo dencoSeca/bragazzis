8c19d5cb03349579833039e48c622b93dcc5622b
dencoSeca <leon.brown71@gmail.com>
2020-11-17 11:05:07 +0000

    clean up imports on Page component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import gsap from 'gsap'
 
 function Page({ title, children }) {
@@ -16,14 +16,14 @@ function Page({ title, children }) {
 
   return (
     <>
-        <div className="page">
-          <div className="container">
-            <div className="title">
-              <h1>{title}</h1>
-            </div>
-            <div className="content">{children}</div>
+      <div className="page">
+        <div className="container">
+          <div className="title">
+            <h1>{title}</h1>
           </div>
+          <div className="content">{children}</div>
         </div>
+      </div>
     </>
   )
 }
