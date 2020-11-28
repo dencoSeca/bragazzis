a265dd7d010ce536d801b34e0e14bee53f062035
dencoSeca <leon.brown71@gmail.com>
2020-11-28 14:43:12 +0000

    style title for new page layout

--- src/components/Page.js
+++ src/components/Page.js
@@ -8,38 +8,36 @@ function Page({ title, children }) {
   return (
     <>
       <div className="page">
-        <div className="container">
-          <motion.h1
-            className="title"
-            initial={{ opacity: 0, y: 48 }}
-            animate={{
-              opacity: 1,
-              y: 0
-            }}
-            exit={{
-              opacity: 0,
-              y: -48
-            }}
-            transition={{ duration: 0.4, delay: 0.1, ease: ease.smooth }}
-          >
-            {title}
-          </motion.h1>
-          <motion.div
-            className="content"
-            initial={{ opacity: 0, y: 48 }}
-            animate={{
-              opacity: 1,
-              y: 0
-            }}
-            exit={{
-              opacity: 0,
-              y: -48
-            }}
-            transition={{ duration: 0.4, delay: 0.2, ease: ease.smooth }}
-          >
-            {children}
-          </motion.div>
-        </div>
+        <motion.h1
+          className="title"
+          initial={{ opacity: 0, y: 48 }}
+          animate={{
+            opacity: 1,
+            y: 0
+          }}
+          exit={{
+            opacity: 0,
+            y: -48
+          }}
+          transition={{ duration: 0.4, delay: 0.1, ease: ease.smooth }}
+        >
+          {title}
+        </motion.h1>
+        <motion.div
+          className="content"
+          initial={{ opacity: 0, y: 48 }}
+          animate={{
+            opacity: 1,
+            y: 0
+          }}
+          exit={{
+            opacity: 0,
+            y: -48
+          }}
+          transition={{ duration: 0.4, delay: 0.2, ease: ease.smooth }}
+        >
+          {children}
+        </motion.div>
       </div>
     </>
   )
