5730cc989608cb03af67b3fa460b722878384503
dencoSeca <leon.brown71@gmail.com>
2021-01-04 15:14:02 +0000

    update ourStory page style and copy

--- src/pages/OurStory.js
+++ src/pages/OurStory.js
@@ -3,57 +3,47 @@ import React from 'react'
 // Components
 import Page from '../components/Page'
 
-// Assets
-import pageBanner from '../assets/images/shop-01.jpg'
-
 function OurStory() {
   return (
     <>
       <Page title={'Our Story'}>
-        <div className="our-story-screen">
-          <div className="row no-gutters">
-            <div className="our-story-screen__page-banner col-md-6">
-              <img src={pageBanner} alt="" />
-            </div>
-            <div className="our-story-screen__content col-md-6">
-              <h4>History</h4>
-              <p>
-                Bragazzi’s opened in Sheffield in 2003, owned by Matteo
-                Bragazzi. Bragazzi’s was (perhaps is) an outlier and safe haven
-                for people who like dry ham and little cups of coffee.
-              </p>
-              <p>
-                Matteo has a brother, Dino. They often holiday together and were
-                in Rome one year when Matteo drifted into a melancholy state of
-                mind. Sorry to see him this way, Dino started up a monologue on
-                a history of Italian dining in London. Their dad had come over,
-                like so many others, and made a business of food selling. As
-                Dino reached a point about napkins, Matteo recognised his fate
-                as the same.
-              </p>
-              <p>
-                He interrupted mid sentence and asked for five hundred euros.
-                They did a big shop of crisps and pasta and floated it to
-                England, ready for the cafe to come.
-              </p>
-              <h4>What we do</h4>
-              <p>
-                Bragazzi’s is a cafe, delicatessen and shop. We sell Italian
-                perishables and dry goods, all of which are good to eat. Most
-                people come for the sandwiches, which are potent assemblies of
-                D.O.C cheese, salami and preserved vegetables.
-              </p>
-              <p>
-                At breakfast, we have pastries. In summer we have gelato. In
-                winter, we have shelves of hard-to-find Christmas produce direct
-                from producers in Italy.
-              </p>
-              <h4>Services</h4>
-              <p>
-                Bragazzi’s cater for all functions. All of our food is made
-                in-house and shaped around what you need for your event.
-              </p>
-            </div>
+        <div className="container">
+          <div className="our-story__content">
+            <h4>History</h4>
+            <p>
+              Bragazzi’s opened in Sheffield in 2003 and is owned by Matteo
+              Bragazzi. Bragazzi’s was (perhaps is) an outlier and safe haven
+              for people who enjoy the “qualcosa in più“.
+            </p>
+            <p>
+              Matteo has a brother, Dino, they often holiday together. In Rome
+              one evening, enjoying a Shakerato, Matteo’s mind drifted. Sorry to
+              see him this way, Dino started up a monologue on their family
+              history of Italian dining in London. Their father had come over,
+              like so many others, and made a business of selling food. As Dino
+              reached a point about the Corradi brothers, Matteo recognised his
+              fate as the same. And so, the bet was placed over a plastic table,
+              outside a bar in Fiano Romano on that hot evening in 2002. They
+              did a big shop with help from Zia Maria and floated it to England,
+              ready for the cafe to come.
+            </p>
+            <h4>What we do</h4>
+            <p>
+              Bragazzi’s is a cafe, delicatessen and shop. We sell Italian
+              perishables and dry goods, all of which are good to eat. Most
+              people come for the sandwiches, which are potent assemblies of
+              D.O.C cheese, salami and preserved vegetables.
+            </p>
+            <p>
+              At breakfast, we have pastries. In summer we have gelato. In
+              winter, we have shelves of hard-to-find Christmas produce direct
+              from producers in Italy.
+            </p>
+            <h4>Services</h4>
+            <p>
+              Bragazzi’s cater for all functions. All of our food is made
+              in-house and shaped around what you need for your event.
+            </p>
           </div>
         </div>
       </Page>
