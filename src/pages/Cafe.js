f5dd09badff4e82c12376f9ae304dc614b4aff4c
dencoSeca <leon.brown71@gmail.com>
2021-01-08 12:24:51 +0000

    add cafe page copy

--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -9,7 +9,12 @@ function Cafe({ openingHours }) {
       <Page title={'Cafe'}>
         <div className="cafe-screen__background-image">
           <div className="cafe-screen__background-image__tint"></div>
-          
+          <p>
+            We use our own carefully curated blend roasted by Darkwoods coffee,
+            West Yorkshire. We only use specialty graded coffee, which has a
+            cleaner and more distinctive flavour than cheaper commercial coffee,
+            and is traceable back to the skilled farmers that produce it.
+          </p>
         </div>
       </Page>
     </>
