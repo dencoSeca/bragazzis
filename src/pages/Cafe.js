eff6c7afeee9a6aa3b5a844b8759712e7ee9ba23
dencoSeca <leon.brown71@gmail.com>
2021-01-09 15:42:13 +0000

    add covid restrictions section on Cafe page

--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -15,6 +15,29 @@ function Cafe({ openingHours }) {
             cleaner and more distinctive flavour than cheaper commercial coffee,
             and is traceable back to the skilled farmers that produce it.
           </p>
+          <div className="cafe-screen__covid">
+            <h4>Covid Restrictions</h4>
+            <p>
+              We want to reassure you that we are putting all possible measures
+              in place to keep you and our team safe so that you can confidently
+              enjoy your visit.
+            </p>
+            <p>
+              We’re following all government guidelines and working on best
+              practice measures within the shops. We have completed all
+              necessary risk assessments and all procedures and assessments will
+              be constantly reviewed and updated where necessary in line with
+              government guidelines.
+            </p>
+            <p>
+              We ask you to please use common sense to play your part in keeping
+              everyone safe. In return we will be doing the same for you.
+            </p>
+            <p>
+              Due to the constantly changing nature of this guidance, we will be
+              continuously reviewing our procedures related to capacity.
+            </p>
+          </div>
         </div>
       </Page>
     </>
