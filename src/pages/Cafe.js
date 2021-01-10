17ffac336847f54c2bae107aea74cb3de6b673b4
dencoSeca <leon.brown71@gmail.com>
2021-01-10 12:05:43 +0000

    add cafe page conversation

--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -8,12 +8,28 @@ function Cafe({ openingHours }) {
     <>
       <Page title={'Cafe'}>
         <div className="container">
-          <p>
-            We use our own carefully curated blend roasted by Darkwoods coffee,
-            West Yorkshire. We only use specialty graded coffee, which has a
-            cleaner and more distinctive flavour than cheaper commercial coffee,
-            and is traceable back to the skilled farmers that produce it.
-          </p>
+          <div className="cafe-screen__ethos">
+            <div className="conversation">
+              <div className="conversation__left-wrapper">
+                <span className="conversation__line">What d'you want?</span>
+              </div>
+              <div className="conversation__right-wrapper">
+                <span className="conversation__line">What've you got?</span>
+              </div>
+              <div className="conversation__left-wrapper">
+                <span className="conversation__line">
+                  Let me make you something...
+                </span>
+              </div>
+            </div>
+            <p>
+              We use our own carefully curated blend roasted by Darkwoods
+              coffee, West Yorkshire. We only use specialty graded coffee, which
+              has a cleaner and more distinctive flavour than cheaper commercial
+              coffee, and is traceable back to the skilled farmers that produce
+              it.
+            </p>
+          </div>
           <div className="cafe-screen__covid">
             <h4>Covid Restrictions</h4>
             <p>
@@ -44,5 +60,3 @@ function Cafe({ openingHours }) {
 }
 
 export default Cafe
-
-
