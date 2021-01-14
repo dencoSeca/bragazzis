8bdfdef843386efbcb667ccb7729bf79f22ba0ad
dencoSeca <leon.brown71@gmail.com>
2021-01-14 16:15:38 +0000

    style cafe page

--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -5,43 +5,29 @@ import Page from '../components/Page'
 
 // Assets
 import sandwichesImg from '../assets/images/bragazzis-cafe-7.jpg'
+import carImg from '../assets/images/bragazzis-cafe-1.jpg'
 
 function Cafe({ openingHours }) {
   return (
     <>
       <Page title={'Caffè'}>
-        <div className="container">
-          <div className="cafe-ethos">
-            <p>
-              We use our own carefully curated blend roasted by coffee, West
-              Yorkshire. We only use specialty graded which has a cleaner and
-              more distinctive flavour than commercial coffee, and is traceable
-              back to the skilled that produce it.
-            </p>
-            <p>
-              Bragazzi’s is nothing without people. You have your regulars, and
-              they have their usual orders. We are first and foremost an
-              exercise in real-life shopkeeping and we treasure the analogue
-              charm of a brick-and-mortar shop.
-            </p>
-            <p>
-              We relish the opportunity to serve you in person and talk of
-              espresso or the preparation of a sugo.
-            </p>
-            <p>
-              We are a small team of people, with different interests and
-              experiences, but with a common appreciation for the somewhat
-              overlooked and at times undervalued occupation of shopkeeping and
-              the unrelenting pursuit of making good coffee.
-            </p>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="cafe__opening-hours-wrapper">
+              {openingHours.map((data, index) => (
+                <div className="cafe__opening-hours-line" key={index}>
+                  {data}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="banner__wrapper">
-          <img
-            src={sandwichesImg}
-            alt="open sandwiches full of roast italian vegetables"
-            className="banner__img"
-          />
+          <div className="col-md-6">
+            <img
+              className="cafe__toy-car-img"
+              src={carImg}
+              alt="old italian wooden toy car in cafe window"
+            />
+          </div>
         </div>
       </Page>
     </>
