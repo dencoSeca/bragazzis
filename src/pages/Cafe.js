6ed06731a607e37e22bf732ac447dad4abad4bfd
dencoSeca <leon.brown71@gmail.com>
2021-01-14 16:21:53 +0000

    style cafe page

--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -6,12 +6,13 @@ import Page from '../components/Page'
 // Assets
 import sandwichesImg from '../assets/images/bragazzis-cafe-7.jpg'
 import carImg from '../assets/images/bragazzis-cafe-1.jpg'
+import craigImg from '../assets/images/bragazzis-cafe-10.jpg'
 
 function Cafe({ openingHours }) {
   return (
     <>
       <Page title={'Caffè'}>
-        <div className="row">
+        <div className="row no-gutters">
           <div className="col-md-6">
             <div className="cafe__opening-hours-wrapper">
               {openingHours.map((data, index) => (
@@ -23,11 +24,17 @@ function Cafe({ openingHours }) {
           </div>
           <div className="col-md-6">
             <img
-              className="cafe__toy-car-img"
+              className="cafe__banner-img"
               src={carImg}
               alt="old italian wooden toy car in cafe window"
             />
           </div>
+        </div>
+        <div className="row no-gutters">
+          <div className="col-md-6">
+            <img src={craigImg} alt="shelves full of italian dry goods... and craig" className="cafe__banner-img"/>
+          </div>
+          <div className="col-md-6"></div>
         </div>
       </Page>
     </>
