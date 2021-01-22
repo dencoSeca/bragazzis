6477ae83456b88904fa42cb420b9e7664415e0fe
dencoSeca <leon.brown71@gmail.com>
2021-01-22 15:59:27 +0000

    add copy to shop page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -7,11 +7,22 @@ function Shop() {
   return (
     <>
       <Page title={'Il Negozio'} color="#019A57">
-        <p>
-          We trade directly with suppliers in Italy. We choose to work with
-          suppliers who focus on the quality, integrity and provenance of their
-          produce. Year round we sell a wide range of everyday staple foods.
-        </p>
+        <div className="content-card">
+          <p>
+            We trade directly with suppliers in Italy. We choose to work with
+            suppliers who focus on the quality, integrity and provenance of
+            their produce. Year round we sell a wide range of everyday staple
+            foods.
+          </p>
+        </div>
+        <div className="content-card">
+          <p>
+            We are a small team of people, with different interests and
+            experiences, but with a common appreciation for the somewhat
+            overlooked, and at times undervalued occupation of shopkeeping, and
+            the unrelenting pursuit of making good coffee.
+          </p>
+        </div>
       </Page>
     </>
   )
